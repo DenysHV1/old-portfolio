f62d9609a47340b17f65d04cd3009abf11c6f60e
DenVik <denyagarkusha2@gmail.com>
2024-09-18 16:11:48 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -8,19 +8,16 @@ import { jsFeatures } from './js/jsFeatures';
 const jsFeaturesEl = document.querySelector('.js-features-list');
 const featuresBtn = document.querySelector('.js-features-title');
 let click1 = 0;
+jsFeatures(projectArr, jsFeaturesEl);
 featuresBtn.addEventListener('click', () => {
 	if (click1 === 0) {
-		jsFeatures(projectArr, jsFeaturesEl);
-		jsFeaturesEl.style.display = 'flex';
-		jsFeaturesEl.style.paddingTop = '20px';
-		jsFeaturesEl.style.paddingBottom = '60px';
+		jsFeaturesEl.classList.add('is-open');
 		click1 += 1;
 	} else {
-		jsFeaturesEl.style.display = 'none';
-		jsFeaturesEl.style.paddingTop = '0';
-		jsFeaturesEl.style.paddingBottom = '0';
+		jsFeaturesEl.classList.remove('is-open');
 		click1 = 0;
 	}
+
 });
 
 //todo -------------------------------------
@@ -29,19 +26,17 @@ const jsFormsEl = document.querySelector('.js-forms-list');
 
 const formsBtn = document.querySelector('.js-forms-title');
 let click2 = 0;
+
+jsForms(projectArr, jsFormsEl);
 formsBtn.addEventListener('click', () => {
 	if (click2 === 0) {
-		jsForms(projectArr, jsFormsEl);
-		jsFormsEl.style.display = 'flex';
-		jsFormsEl.style.paddingTop = '20px';
-		jsFormsEl.style.paddingBottom = '60px';
+		jsFormsEl.classList.add('is-open');
 		click2 += 1;
 	} else {
-		jsFormsEl.style.display = 'none';
-		jsFormsEl.style.paddingTop = '0';
-		jsFormsEl.style.paddingBottom = '0';
+		jsFormsEl.classList.remove('is-open');
 		click2 = 0;
 	}
+
 });
 
 //todo -------------------------------------
@@ -51,19 +46,17 @@ const jsGamesEl = document.querySelector('.js-games-list');
 
 const gamesBtn = document.querySelector('.js-games-title');
 let click3 = 0;
+
+jsGames(projectArr, jsGamesEl);
 gamesBtn.addEventListener('click', () => {
 	if (click3 === 0) {
-		jsGames(projectArr, jsGamesEl);
-		jsGamesEl.style.display = 'flex';
-		jsGamesEl.style.paddingTop = '20px';
-		jsGamesEl.style.paddingBottom = '60px';
+		jsGamesEl.classList.add('is-open');
 		click3 += 1;
 	} else {
-		jsGamesEl.style.display = 'none';
-		jsGamesEl.style.paddingTop = '0';
-		jsGamesEl.style.paddingBottom = '0';
+		jsGamesEl.classList.remove('is-open');
 		click3 = 0;
 	}
+
 });
 
 //todo -------------------------------------
@@ -73,19 +66,17 @@ const jsProjectsEl = document.querySelector('.js-projects-list');
 
 const jsProjectsBtn = document.querySelector('.js-projects-title');
 let click4 = 0;
+
+jsProjects(projectArr, jsProjectsEl);
 jsProjectsBtn.addEventListener('click', () => {
 	if (click4 === 0) {
-		jsProjects(projectArr, jsProjectsEl);
-		jsProjectsEl.style.display = 'flex';
-		jsProjectsEl.style.paddingTop = '20px';
-		jsProjectsEl.style.paddingBottom = '60px';
+		jsProjectsEl.classList.add('is-open');
 		click4 += 1;
 	} else {
-		jsProjectsEl.style.display = 'none';
-		jsProjectsEl.style.paddingTop = '0';
-		jsProjectsEl.style.paddingBottom = '0';
+		jsProjectsEl.classList.remove('is-open');
 		click4 = 0;
 	}
+
 });
 
 //todo -------------------------------------
@@ -95,19 +86,17 @@ const simpleWebSidesEl = document.querySelector('.simple-webSides-list-js');
 
 const webSidesBtn = document.querySelector('.simple-websides-title');
 let click5 = 0;
+
+simpleWebSides(projectArr, simpleWebSidesEl);
 webSidesBtn.addEventListener('click', () => {
 	if (click5 === 0) {
-		simpleWebSides(projectArr, simpleWebSidesEl);
-		simpleWebSidesEl.style.display = 'flex';
-		simpleWebSidesEl.style.paddingTop = '20px';
-		simpleWebSidesEl.style.paddingBottom = '60px';
+		simpleWebSidesEl.classList.add('is-open');
 		click5 += 1;
 	} else {
-		simpleWebSidesEl.style.display = 'none';
-		simpleWebSidesEl.style.paddingTop = '0';
-		simpleWebSidesEl.style.paddingBottom = '0';
+		simpleWebSidesEl.classList.remove('is-open');
 		click5 = 0;
 	}
+
 });
 
 //todo -------------------------------------
@@ -117,19 +106,17 @@ const reactListEl = document.querySelector('.react-list');
 
 const reactBtn = document.querySelector('.react-title');
 let click6 = 0;
+
+reactProjects(projectArr, reactListEl);
 reactBtn.addEventListener('click', () => {
 	if (click6 === 0) {
-		reactProjects(projectArr, reactListEl);
-		reactListEl.style.display = 'flex';
-		reactListEl.style.paddingTop = '20px';
-		reactListEl.style.paddingBottom = '60px';
+		reactListEl.classList.add('is-open');
 		click6 += 1;
 	} else {
-		reactListEl.style.display = 'none';
-		reactListEl.style.paddingTop = '0';
-		reactListEl.style.paddingBottom = '0';
+		reactListEl.classList.remove('is-open');
 		click6 = 0;
 	}
+
 });
 
 //todo --------------------------------------
